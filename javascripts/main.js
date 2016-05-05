7e64f9ba1a4ab17429bd6f4c16feea324b54276a
Rakesh Falke <rakesh@agsft.com>
2016-05-05 14:34:33 +0530

    Added service worker

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,5 @@
-console.log('This would be the main JS file.');
+navigator.serviceWorker.register('/worker.js').then(function(reg) {
+  console.log('◕‿◕', reg);
+}, function(err) {
+  console.log('ಠ_ಠ', err);
+});
