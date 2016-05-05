b7ac83761cd5e3be2a4a505fe3eca026135ab1c7
Rakesh Falke <rakesh@agsft.com>
2016-05-05 14:51:11 +0530

    Javascript changes

--- javascripts/worker.js
+++ javascripts/worker.js
@@ -1,16 +1,11 @@
 // The SW will be shutdown when not in use to save memory,
 // be aware that any global state is likely to disappear
-console.log("SW startup");
-
 self.addEventListener('install', function(event) {
-  console.log("SW installed");
-});
+  console.log("Installing…");
 
-self.addEventListener('activate', function(event) {
-  console.log("SW activated");
-});
-
-self.addEventListener('fetch', function(event) {
-  console.log("Caught a fetch!");
-  event.respondWith(new Response("Hello world!"));
+  event.waitUntil(
+    somethingThatReturnsAPromise().then(function() {
+      console.log("Installed!");
+    })
+  );
 });
