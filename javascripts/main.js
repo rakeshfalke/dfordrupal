7118c955644fb2b7017119483cd2be1d60596c83
Rakesh Falke <rakesh@agsft.com>
2016-05-05 14:48:07 +0530

    Javascript changes

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,4 +1,4 @@
-navigator.serviceWorker.register('worker.js').then(function(reg) {
+navigator.serviceWorker.register('javascripts/worker.js').then(function(reg) {
   console.log("SW Ok",reg);
 }, function(err) {
   console.log("SW Error",err);
